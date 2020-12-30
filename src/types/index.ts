af546dfe8d2d1e8f5732127305be9e367d31e1cf
Chris <christianotu.d@gmail.com>
2020-12-30 14:19:35 -0800

    Types for Redux state

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,16 @@
 import { RootState } from "../slices";
 
 export type { RootState };
+
+export type Ids = Array<number>;
+
+export interface TodoItem {
+  id: number,
+  title: string;
+  description: string;
+}
+
+export interface State {
+  ids: Ids;
+  item: Record<number, TodoItem>;
+}
