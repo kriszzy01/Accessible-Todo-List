f87c7c95c774fb015fc41c1b9f6d54373deeef41
Chris <christianotu.d@gmail.com>
2020-12-30 21:53:39 -0800

    Reducer Logic

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -1,7 +1,12 @@
 import { combineReducers } from "redux";
+import todoReducer from "./todoSlice";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  todo: todoReducer,
+});
 
 export type RootState = ReturnType<typeof rootReducer>;
+
+export * from "./todoSlice";
 
 export default rootReducer;
