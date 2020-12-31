648f009a7b067097ce460920c4541d2ce0bbca0d
Chris <christianotu.d@gmail.com>
2020-12-30 21:53:21 -0800

    Create todoSlice

---
+++ src/slices/todoSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { State, TodoItem } from "../types";
+
+const initialState: State = {
+  ids: [1, 2],
+  item: {
+    1: { id: 1, description: "Remember to buy eggs" },
+    2: { id: 2, description: "Learn Haskel" },
+  },
+};
+
+const todo = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    deleteItem(state, { payload }: PayloadAction<number>) {
+      const filteredIds = state.ids.filter((id) => id !== payload);
+      state.ids = filteredIds;
+
+      let filteredItems = {};
+
+      filteredIds.forEach((id) => {
+        filteredItems = {
+          ...filteredItems,
+          [id]: state.item[id],
+        };
+      });
+
+      state.item = filteredItems;
+    },
+    addItem(state, { payload }: PayloadAction<TodoItem>) {
+      state.ids.push(payload.id);
+
+      state.item = {
+        ...state.item,
+        [payload.id]: payload,
+      };
+    },
+  },
+});
+
+export const { deleteItem, addItem } = todo.actions;
+
+export default todo.reducer;
